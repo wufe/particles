170018e4a98d479ed7d22d2d36025ea539669411
Simone Bembi <simone.bembi@gmail.com>
2020-02-19 21:23:11 +0100

    Circle in webgl

--- src/webgl/programs/shaders/particles/particles.fs.ts
+++ src/webgl/programs/shaders/particles/particles.fs.ts
@@ -4,6 +4,12 @@ precision mediump float;
 varying vec4 frag_col;
 
 void main() {
-    gl_FragColor = frag_col;
+    float r = 0.0, delta = 0.0, alpha = 1.0;
+    vec2 cxy = 2.0 * gl_PointCoord - 1.0;
+    r = dot(cxy, cxy);
+    if (r > 1.0) {
+        discard;
+    }
+    gl_FragColor = frag_col * (alpha);
 }
 `;
