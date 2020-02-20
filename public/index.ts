22ae1ef86380e24102f87fe2ab1d970c1853940e
Simone Bembi <simone.bembi@gmail.com>
2020-02-20 22:29:50 +0100

    Created liquid particle system

--- public/index.ts
+++ public/index.ts
@@ -1,9 +1,11 @@
 import { init } from '../src/main';
 import { RendererWebGL } from '../src/rendering/renderer-webgl';
+import { LiquidParticleSystemBuilder } from '../src/systems/liquid-particle-system';
 
 init({
     selectorOrCanvas: '#canvas',
     renderer: RendererWebGL,
+    systems: [LiquidParticleSystemBuilder.build({particles:{background:{count:30}}})],
     camera: {
         enabled: true
     }
---
+++ src/systems/liquid-particle-system.ts
@@ -0,0 +1,43 @@
+import { BaseParticleSystem } from "./base-particle-system";
+import { IParticleSystem } from "../models/particle-system";
+import { IParticle, Particle } from "../models/particle";
+import { Vector3D } from "../models/vector3d";
+
+export interface ILiquidParticleSystemParams {
+    particles: {
+        background: {
+            count: number;
+        }
+    }
+}
+
+export class LiquidParticleSystemBuilder {
+
+    public static build(params: ILiquidParticleSystemParams) {
+        return class extends BaseParticleSystem implements IParticleSystem {
+
+            private _particles: IParticle[] = [];
+
+            attach() {
+                const { width, height } = this.manager.internals;
+
+                this._particles = new Array(params.particles.background.count)
+                    .fill(null)
+                    .map(_ => {
+                        const x = Math.random() * 500 - 250;
+                        const y = Math.random() * 500 - 250;
+                        const z = 0;
+                        const particle = new Particle(new Vector3D({ x, y, z }), this.manager);
+                        particle.size = 20 + Math.random() * 30;
+                        particle.color.w = Math.random() / 2 + .5;
+                        return particle;
+                    });
+            }
+
+            getParticles() {
+                return this._particles;
+            }
+        }
+    }
+
+}
