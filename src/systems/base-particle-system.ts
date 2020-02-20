c0588a737c7e1825a0bdeef553636e109aa53f58
Simone Bembi <simone.bembi@gmail.com>
2020-02-20 07:36:14 +0100

    Added base particle system

---
+++ src/systems/base-particle-system.ts
@@ -0,0 +1,16 @@
+import { ILibraryInterface } from "../main";
+import { SystemBridgeEventNotification } from "../drawing/system-bridge";
+import { IParticleSystem } from "../models/particle-system";
+import { IParticle } from "../models/particle";
+
+export abstract class BaseParticleSystem implements IParticleSystem {
+    abstract attach(): void;
+    abstract getParticles(): IParticle[];
+
+    constructor(protected manager: ILibraryInterface) {}
+
+    notifyWholeSystemChanged() {
+        if (this.manager)
+            this.manager.notify(SystemBridgeEventNotification.CHANGE, this);
+    }
+}
